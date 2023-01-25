9036c2bba78e6e479861fd712696e5a814c7f8c7
tiszapeter <tiszapeter82@gmail.com>
2023-01-25 19:08:57 +0100

    Initial commit

---
+++ src/test/java/com/example/toll_starter/TollStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toll_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
