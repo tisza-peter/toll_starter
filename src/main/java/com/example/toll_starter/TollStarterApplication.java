67d060720cfafca48616442c5e74b699dbbb5c4d
tiszapeter <tiszapeter82@gmail.com>
2023-01-25 19:11:28 +0100

    init

--- src/main/java/com/example/toll_starter/TollStarterApplication.java
+++ src/main/java/com/example/toll_starter/TollStarterApplication.java
@@ -1,13 +1,42 @@
 package com.example.toll_starter;
+import com.example.toll_core.listvignettaforvehicle.ListOfVehicleWithVignettes;
+import com.example.toll_core.listvignettaforvehicle.api.VehicleWithVignettesResponsePresenter;
+import com.example.toll_core.listvignettaforvehicle.storage.VignettesStorage;
+import com.example.toll_cui.TollSystemView;
+import com.example.toll_cui.listvignettaforvehicle.Controller;
+import com.example.toll_cui.listvignettaforvehicle.Presenter;
+import com.example.toll_db_store.VignettesDbStoreApplication;
+import com.example.vehicle_core.VehicleCoreApplication;
+import com.example.vehicle_core.VehicleStorage;
+import com.example.vehicle_filedirectory_store.Store_Repository_Implementation;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TollStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TollStarterApplication.class, args);
-    }
+        ListOfVehicleWithVignettes tollCore = new ListOfVehicleWithVignettes();
 
+        TollSystemView view= new TollSystemView();
+
+        Presenter presenterImpl = new Presenter();
+        presenterImpl.setView(view);
+
+        VehicleWithVignettesResponsePresenter vehicleWithVignettesResponsePresenter = presenterImpl;
+        tollCore.setUIPresenter(vehicleWithVignettesResponsePresenter);
+
+        VignettesStorage vignettesStorage = new VignettesDbStoreApplication();
+        tollCore.SetStore(vignettesStorage);
+
+        VehicleCoreApplication vehicleCore = new VehicleCoreApplication();
+        VehicleStorage vehicleRepository = new Store_Repository_Implementation();
+        vehicleCore.SetStore(vehicleRepository);
+
+        tollCore.SetVehicleCoreInteractor(vehicleCore);
+        vehicleCore.SetUI(tollCore);
+
+        Controller controller = new Controller();
+        controller.SetCore(tollCore);
+
+        view.SetController(controller);
+        view.Start();
+    }
 }
--- src/test/java/com/example/toll_starter/TollStarterApplicationTests.java
+++ src/test/java/com/example/toll_starter/TollStarterApplicationTests.java
@@ -1,9 +1,6 @@
 package com.example.toll_starter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class TollStarterApplicationTests {
 
     @Test
